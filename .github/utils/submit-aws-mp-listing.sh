f9d5294fc20dadb12c1cf5017df4c9550716da16
Sayali M <sayali@Sayalis-MacBook-Pro>
2024-10-21 09:24:39 -0500

    test visibility

--- .github/utils/submit-aws-mp-listing.sh
+++ .github/utils/submit-aws-mp-listing.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+if [ "$DRY_RUN" == "false" ]; then
 # Construct a JSON string with details about the new version to be submitted to AWS Marketplace
 # This script is intended to be run as part of a GitHub Actions workflow
 DETAILS_JSON=$(cat <<EOF
@@ -28,16 +29,40 @@ DETAILS_JSON=$(cat <<EOF
 }
 EOF
 );
-
-# Convert the JSON string to a single line for the AWS CLI command
-DETAILS_JSON_STRING="$(echo "${DETAILS_JSON}" | jq -c 'tostring';)";
-
-# Determine visibility based on dry run
-if [ "$DRY_RUN" == "true" ]; then
-  VISIBILITY="Restricted"
 else
-  VISIBILITY="Public"
+
+  # FOR DRY_RUN : Construct a JSON string with details about the new version to be submitted to AWS Marketplace
+  # This script is intended to be run as part of a GitHub Actions workflow
+  DETAILS_JSON=$(cat <<EOF
+{
+    "Version": {
+        "VersionTitle": "$IMAGE_TAG",
+        "ReleaseNotes": "https://docs.liquibase.com/start/release-notes/liquibase-release-notes/liquibase-$IMAGE_TAG.html"
+      },
+      "DeliveryOptions": [
+        {
+          "DeliveryOptionTitle": "Liquibase Pro Docker Image",
+          "Details": {
+            "EcrDeliveryOptionDetails": {
+              "ContainerImages": [
+                "$AWS_MP_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG"
+              ],
+              "CompatibleServices": [
+                "ECS", "EKS"
+              ],
+              "Description":"This is the official Docker image for Liquibase Pro.",
+              "UsageInstructions":"Product install instructions are available at https://contribute.liquibase.com/extensions-integrations/directory/integration-docs/liquibase-container-aws-ecs-eks/"
+              }
+          }
+        }
+      ],
+       "Visibility": "Restricted"
+  }
+  EOF
+  );
 fi
+# Convert the JSON string to a single line for the AWS CLI command
+DETAILS_JSON_STRING="$(echo "${DETAILS_JSON}" | jq 'tostring';)";
 
 # Notify user of the submission process
 echo "Submitting new version for verification";
@@ -48,13 +73,11 @@ aws marketplace-catalog start-change-set \
     --region "${AWS_REGION}" \
     --change-set '[
       {
-        "ChangeType": "AddDeliveryOptions",
+        "ChangeType": "'"$( [ "$DRY_RUN" == "true" ] && echo "UpdateVisibility" || echo "AddDeliveryOptions" )"'",
         "Entity": {
           "Identifier": "'"${PRODUCT_ID}"'",
           "Type": "ContainerProduct@1.0"
         },
-        "Details": '"${DETAILS_JSON_STRING}"',
-          "TargetVisibility": "'"${VISIBILITY}"'"
-        }
+        "Details": '"${DETAILS_JSON_STRING}"'
+      }
       ]';
-
