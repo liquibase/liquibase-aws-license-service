bf519941f7dcfb52a922792a43e9d9a3447e2534
Sayali M <sayali@Sayalis-MacBook-Pro>
2024-10-22 15:28:06 -0500

    Submit New Version Request for AWS MP Listing

--- .github/utils/submit-aws-mp-listing.sh
+++ .github/utils/submit-aws-mp-listing.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 
 # Construct a JSON string with details about the new version to be submitted to AWS Marketplace
-# This script is intended to be run as part of a GitHub Actions workflow
 DETAILS_JSON=$(cat <<EOF
 {
     "Version": {
         "VersionTitle": "${IMAGE_TAG}",
         "ReleaseNotes": "https://docs.liquibase.com/start/release-notes/liquibase-release-notes/liquibase-${IMAGE_TAG}.html"
-      },
-      "DeliveryOptions": [
+    },
+    "DeliveryOptions": [
         {
-          "DeliveryOptionTitle": "Liquibase Pro Docker Image",
-          "Details": {
-            "EcrDeliveryOptionDetails": {
-              "ContainerImages": [
-                "${ECR_REPOSITORY}:${IMAGE_TAG}"
-              ],
-              "CompatibleServices": [
-                "ECS", "EKS"
-              ],
-              "Description":"This is the official Docker image for Liquibase Pro.",
-              "UsageInstructions":"Product install instructions are available at https://contribute.liquibase.com/extensions-integrations/directory/integration-docs/liquibase-container-aws-ecs-eks/"
-              }
-          }
+            "DeliveryOptionTitle": "Liquibase Pro Docker Image",
+            "Details": {
+                "EcrDeliveryOptionDetails": {
+                    "ContainerImages": [
+                        "${ECR_REPOSITORY}:${IMAGE_TAG}"
+                    ],
+                    "CompatibleServices": [
+                        "ECS", "EKS"
+                    ],
+                    "Description": "This is the official Docker image for Liquibase Pro.",
+                    "UsageInstructions": "Product install instructions are available at https://contribute.liquibase.com/extensions-integrations/directory/integration-docs/liquibase-container-aws-ecs-eks/"
+                }
+            }
         }
-      ]
+    ]
 }
 EOF
-);
+)
 
-# Convert the JSON string to a single line for the AWS CLI command
-DETAILS_JSON_STRING="$(echo "${DETAILS_JSON}" | jq -c .)";
+# Convert the JSON string to a single line and escape quotes for the AWS CLI command
+DETAILS_JSON_STRING=$(echo "${DETAILS_JSON}" | jq -c . | sed 's/"/\\"/g')
 
 # Notify user of the submission process
-echo "Submitting new version for verification";
+echo "Submitting new version for verification"
 
 # Submit the new version for verification
 aws marketplace-catalog start-change-set \
@@ -46,6 +45,6 @@ aws marketplace-catalog start-change-set \
           "Identifier": "'"${PRODUCT_ID}"'",
           "Type": "ContainerProduct@1.0"
         },
-        "Details": '"${DETAILS_JSON_STRING}"'
+        "Details": "'"${DETAILS_JSON_STRING}"'"
       }
-      ]';
+    ]'
